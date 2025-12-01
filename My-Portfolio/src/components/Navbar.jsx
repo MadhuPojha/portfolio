6133cf9824b0ac698b2ecc349a4c08d1699dc529
Madhu Priyadarshni Ojha <165268887+MadhuPojha@users.noreply.github.com>
2025-12-01 11:16:13 +0100

    Update Navbar.jsx

--- My-Portfolio/src/components/Navbar.jsx
+++ My-Portfolio/src/components/Navbar.jsx
@@ -1,14 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../CSS/Navbar.module.css";
-import { getImageUrl } from "../utils";
 import menuIcon from '../assets/menuIcon.png';
 
-export default function Navbar(){
+export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  // Detect scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {  // change 50 to whatever scroll threshold you want
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <nav className={styles.navbar}>
-
+    <nav className={`${styles.navbar} ${scrolled ? styles.navScrolled : ""}`}>
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
@@ -20,31 +32,15 @@ export default function Navbar(){
           className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
           onClick={() => setMenuOpen(false)}
         >
-          <li> 
-            <a className={styles.title} href="">
-              Home
-            </a>    
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#experience">Experience</a>
-          </li>
-          <li>
-            <a href="#education">Education</a>
-          </li>
-          <li>
-            <a href="#skills">Skills</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
+          <li><a className={styles.title} href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#education">Education</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
     </nav>
   );
-};
+}
