ced0ee5aa41a1e934c9a5fe66466da3eca271172
MadhuPojha <mpriya1201@gmail.com>
2026-01-08 13:39:24 +0100

    fixed error

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/portfolio/', // 👈 important line for GitHub Pages
   plugins: [react()],
+  base: '/portfolio/', // 👈 important line for GitHub Pages
   css: {
     modules: {
       localsConvention: "camelCase",
